ec37a0eb949684493477a42f85d523b9ad62ad17
ueda <ueda.rina24@gmail.com>
2025-10-08 13:04:34 +0900

    コミットメッセージ

---
+++ src/php01/index03.php
@@ -0,0 +1,22 @@
+<?php
+$item = "JavaScript";
+$item = "PHP";
+echo $item;
+
+<?<php>
+$item = "JavaScript";
+echo $item;
+</php>
+<?<php>
+echo 20;
+echo "こんにちは";
+</php>
+
+<?<php>
+$name1 = "Tanaka";
+$last_name = "Yamada";
+$first_name = "Saburo";
+echo $name1 . "Jiro";
+echo "<br />";
+echo $last_name . $first_name;
+</php>
---
+++ src/php01/index04.php
@@ -0,0 +1,34 @@
+<?php>
+$a = 15;
+$b = 3;
+$c = 10;
+$answer1 = $a + $b;
+$answer2 = $a - $b;
+$answer3 = $a * $b;
+$answer4 = $a / $b;
+$answer5 = $a % $c;
+echo $answer1;
+echo "<br />";
+echo $answer2;
+echo "<br />";
+echo $answer3;
+echo "<br />";
+echo $answer4;
+echo "<br />";
+echo $answer5;
+</php>
+
+<?php>
+$a = 15;
+$b = 3;
+$c = 10;
+$d = 5;
+
+$answer1 = $a + $b - $c + $d;
+$answer2 = $c + $d;
+
+
+echo $answer1;
+echo "<br />";
+echo $answer2;
+</php>
